c6acf61e7461b5dee8295f868b9243ed6554633d
preetbista <bistapreet4@gmail.com>
2023-04-20 09:56:03 +0545

    [#35] initialize project

---
+++ trainee/preetbista/flightcrud/src/main/java/com/flight/flightcrud/FlightcrudApplication.java
@@ -0,0 +1,13 @@
+package com.flight.flightcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightcrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightcrudApplication.class, args);
+    }
+
+}
