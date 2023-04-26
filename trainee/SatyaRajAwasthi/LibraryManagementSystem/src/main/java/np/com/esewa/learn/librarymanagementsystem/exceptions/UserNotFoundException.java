ae887cd82a6c0969aaad54ff9d1170f27c7797e5
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2023-04-26 13:53:00 +0545

    [#35] custom exception when user not found

---
+++ trainee/SatyaRajAwasthi/LibraryManagementSystem/src/main/java/np/com/esewa/learn/librarymanagementsystem/exceptions/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package np.com.esewa.learn.librarymanagementsystem.exceptions;
+
+/**
+ * @author Thinkpad on 4/25/2023
+ * @project LibraryManagementSystem
+ */
+public class UserNotFoundException extends Throwable{
+}
