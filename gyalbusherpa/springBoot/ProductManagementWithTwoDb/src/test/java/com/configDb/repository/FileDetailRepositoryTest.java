da0efcd5236b25cc69dc921c0a367f9452519847
Gyalbu Sherpa <gyalbusherpa555@gmail.com>
2023-05-02 16:07:39 +0545

    [#61] repository testing completed

--- gyalbusherpa/springBoot/ProductManagementWithTwoDb/src/test/java/com/configDb/repository/FileDetailRepositoryTest.java
+++ gyalbusherpa/springBoot/ProductManagementWithTwoDb/src/test/java/com/configDb/repository/FileDetailRepositoryTest.java
@@ -2,16 +2,11 @@ package com.configDb.repository;
 
 import com.configDb.enums.FileStatusEnum;
 import com.configDb.model.FileDetail;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 
-import java.util.Date;
 import java.util.List;
 
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
@@ -19,7 +14,6 @@ import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
 @DataJpaTest
 //@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class FileDetailRepositoryTest {
 
     @Autowired
@@ -29,65 +23,44 @@ class FileDetailRepositoryTest {
     private TestEntityManager entityManager;
 
     @Test
-    @Order(1)
-    public void saveFileDetail(){
-        FileDetail detail = FileDetail.builder()
+    public void testFindByFileStatus() {
+
+        FileDetail fileDetail1 = FileDetail.builder()
+                .filePath("D:/don.csv")
+                .fileStatus(FileStatusEnum.COMPLETE)
+                .build();
+
+        entityManager.persist(fileDetail1);
+
+        FileDetail fileDetail2 = FileDetail.builder()
                 .filePath("D:/don.csv")
                 .fileStatus(FileStatusEnum.PENDING)
-                .failureCount(0)
-                .successCount(1)
                 .build();
 
-        fileDetailRepository.save(detail);
+        entityManager.persist(fileDetail2);
+        entityManager.flush();
 
-        assertThat(detail).hasFieldOrPropertyWithValue("D:/don.csv", 1);
+        List<FileDetail> results = fileDetailRepository.findByFileStatus(FileStatusEnum.COMPLETE);
 
+        assertThat(results.size()).isEqualTo(1);
+        assertThat(results.get(0).getFilePath()).isEqualTo(fileDetail1.getFilePath());
     }
 
     @Test
-    @Order(2)
-    public void getFileDetail(){
-        fileDetailRepository.findAll().forEach(fileDetail -> System.out.println(fileDetail.getFilePath()));
-    }
-
-    @Test
-    public void should_find_all_files(){
-        FileDetail detail = FileDetail.builder()
-                .filePath("D:/don.csv")
-                .fileStatus(FileStatusEnum.PENDING)
-
-                .failureCount(0)
-                .successCount(1)
-
+    public void testFindFileDetailByFilePath() {
+        // create test data
+        FileDetail fileDetail = FileDetail.builder()
+                .filePath("file.txt")
+                .fileStatus(FileStatusEnum.COMPLETE)
                 .build();
-        entityManager.persist(detail);
 
-        FileDetail detail2 = FileDetail.builder()
-                .filePath("D:/don.csv")
-                .fileStatus(FileStatusEnum.PENDING)
-                .failureCount(0)
-                .successCount(1)
-                .build();
-        entityManager.persist(detail2);
+        entityManager.persist(fileDetail);
+        entityManager.flush();
 
-        List<FileDetail> fileDetail = fileDetailRepository.findAll();
-        assertThat(fileDetail).hasSize(2).contains(detail,detail2);
+        FileDetail result = fileDetailRepository.findFileDetailByFilePath("file.txt");
+
+        assertThat(result.getFilePath()).isEqualTo(fileDetail.getFilePath());
+        assertThat(result.getFileStatus()).isEqualTo(fileDetail.getFileStatus());
     }
 
-    @Test
-    public void getFileDetail_whenNoFileDetailFound_returnEmpty(){
-        List<FileDetail> fileDetail = fileDetailRepository.findAll();
-        assertThat(fileDetail).isEmpty();
-    }
-
-    @Test
-    public void findByStatus(){
-        FileDetail detail = FileDetail.builder()
-                .fileStatus(FileStatusEnum.PENDING)
-                .build();
-        entityManager.persist(detail);
-
-        List<FileDetail> file = fileDetailRepository.findByFileStatus(FileStatusEnum.PROCESSING);
-        assertThat(file).hasSize(1).contains(detail);
-    }
 }
