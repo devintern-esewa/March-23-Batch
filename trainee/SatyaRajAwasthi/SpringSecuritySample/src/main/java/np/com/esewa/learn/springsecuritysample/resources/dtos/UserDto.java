871ab6594fc88b11696b19f6475da08e8a1454b8
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2023-05-03 14:17:46 +0545

    [#75] add UserDto class for getting user infos as response and request

--- trainee/SatyaRajAwasthi/SpringSecuritySample/src/main/java/np/com/esewa/learn/springsecuritysample/resources/dtos/UserDto.java
+++ trainee/SatyaRajAwasthi/SpringSecuritySample/src/main/java/np/com/esewa/learn/springsecuritysample/resources/dtos/UserDto.java
@@ -1,7 +1,7 @@
 package np.com.esewa.learn.springsecuritysample.resources.dtos;
 
 /**
- * @author Thinkpad on 5/2/2023
+ * @author SatyaRajAwasth1 on 5/2/2023
  * A helper DTO class for user to carry data on requests and responses
  */
 public class UserDto {
