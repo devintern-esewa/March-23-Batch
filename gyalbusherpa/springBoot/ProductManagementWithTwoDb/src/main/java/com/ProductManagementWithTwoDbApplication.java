46892772a9e21a0688d62080a9d0695206e1a310
Gyalbu Sherpa <gyalbusherpa555@gmail.com>
2023-05-04 15:33:48 +0545

    [#61] code added for excluding application.properties from resource folder and deploy application.properties file from tomcat config folder

--- gyalbusherpa/springBoot/ProductManagementWithTwoDb/src/main/java/com/ProductManagementWithTwoDbApplication.java
+++ gyalbusherpa/springBoot/ProductManagementWithTwoDb/src/main/java/com/ProductManagementWithTwoDbApplication.java
@@ -1,15 +1,32 @@
 package com;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.Properties;
 
 @SpringBootApplication
 @EnableScheduling
-public class ProductManagementWithTwoDbApplication {
+public class ProductManagementWithTwoDbApplication extends SpringBootServletInitializer {
+
+    private static Properties getProps() {
+        Properties properties = new Properties();
+        properties.put("spring.config.location", "file:${catalina.home}/conf/product_application.properties");
+        return properties;
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ProductManagementWithTwoDbApplication.class)
+                .properties(getProps());
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(ProductManagementWithTwoDbApplication.class, args);
+        new SpringApplicationBuilder(ProductManagementWithTwoDbApplication.class)
+                .properties(getProps())
+                .run(args);
     }
 
 }
