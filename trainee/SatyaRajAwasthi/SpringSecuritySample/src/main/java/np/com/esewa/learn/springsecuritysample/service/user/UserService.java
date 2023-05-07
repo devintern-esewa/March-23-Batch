d947a5b66fb3e8754d01738f9564c68a254d04f7
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2023-05-07 13:38:12 +0545

    [#75] add service impl. for finding user by email and username

--- trainee/SatyaRajAwasthi/SpringSecuritySample/src/main/java/np/com/esewa/learn/springsecuritysample/service/user/UserService.java
+++ trainee/SatyaRajAwasthi/SpringSecuritySample/src/main/java/np/com/esewa/learn/springsecuritysample/service/user/UserService.java
@@ -1,8 +1,15 @@
 package np.com.esewa.learn.springsecuritysample.service.user;
 
+import np.com.esewa.learn.springsecuritysample.entity.User;
+import np.com.esewa.learn.springsecuritysample.resources.dtos.UserDto;
+
 /**
  * @author Thinkpad on 5/2/2023
  * @project SpringSecuritySample
  */
-public class UserService {
+public interface UserService {
+    public void saveUser(UserDto userDto);
+    User findUserByEmail(String email);
+    User findUserByUsername(String username);
+
 }
