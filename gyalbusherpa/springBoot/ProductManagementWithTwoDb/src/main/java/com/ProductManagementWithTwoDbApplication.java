a9fba9ff4a9ceb38206fc9ccc54f74fcdc97bc41
Gyalbu Sherpa <gyalbusherpa555@gmail.com>
2023-05-07 14:49:52 +0545

    [#61] caching with redis

--- gyalbusherpa/springBoot/ProductManagementWithTwoDb/src/main/java/com/ProductManagementWithTwoDbApplication.java
+++ gyalbusherpa/springBoot/ProductManagementWithTwoDb/src/main/java/com/ProductManagementWithTwoDbApplication.java
@@ -3,12 +3,14 @@ package com;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Properties;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class ProductManagementWithTwoDbApplication extends SpringBootServletInitializer {
 
     private static Properties getProps() {
--- gyalbusherpa/springBoot/ProductManagementWithTwoDb/src/main/java/com/inventoryDb/dto/ProductDto.java
+++ gyalbusherpa/springBoot/ProductManagementWithTwoDb/src/main/java/com/inventoryDb/dto/ProductDto.java
@@ -6,12 +6,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.io.Serializable;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class ProductDto {
+public class ProductDto implements Serializable {
     private String name;
     private String code;
     private double quantity;
