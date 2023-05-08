c7abab6e7f92932fb145d413ef160fc85cb75b48
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2023-05-08 11:46:18 +0545

    [#91] initialize project: with basic dependencies

---
+++ trainee/SatyaRajAwasthi/samplemicroservices/src/test/java/np/com/esewa/learn/samplemicroservices/SamplemicroservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package np.com.esewa.learn.samplemicroservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplemicroservicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
