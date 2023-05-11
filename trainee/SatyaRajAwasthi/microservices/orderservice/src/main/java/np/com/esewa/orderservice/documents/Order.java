b9d2651f7c90a62573197d95e3e994d15389d369
SatyaRajAwasth1 <satya.211733@ncit.edu.np>
2023-05-11 15:12:53 +0545

    [#94] finish: order-service for ecommerce microservices

---
+++ trainee/SatyaRajAwasthi/microservices/orderservice/src/main/java/np/com/esewa/orderservice/documents/Order.java
@@ -0,0 +1,35 @@
+package np.com.esewa.orderservice.documents;
+
+import jakarta.annotation.Generated;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import np.com.esewa.orderservice.enums.OrderStatus;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.Instant;
+
+/**
+ * @author SatyaRajAwasth1
+ * Written on: 5/10/2023
+ * @project orderservice
+ * database entity instance for orders
+ */
+@Setter
+@Getter
+@Builder
+@Document(collection = "orders")
+public class Order {
+
+    private long id;
+    
+    private long productId;
+
+    private int quantity;
+
+    private Instant orderDate;
+
+    private OrderStatus orderStatus;
+
+    private float amount;
+}
