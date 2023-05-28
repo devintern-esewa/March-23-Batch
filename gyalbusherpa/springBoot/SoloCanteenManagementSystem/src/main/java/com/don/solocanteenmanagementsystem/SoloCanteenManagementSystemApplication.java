eb1bd9898f37ff9c73cce368d9ff40529a2511c7
GyalbuSherpa5 <gyalbusherpa555@gmail.com>
2023-05-28 12:14:15 +0545

    jwt implementation

--- gyalbusherpa/springBoot/SoloCanteenManagementSystem/src/main/java/com/don/solocanteenmanagementsystem/SoloCanteenManagementSystemApplication.java
+++ gyalbusherpa/springBoot/SoloCanteenManagementSystem/src/main/java/com/don/solocanteenmanagementsystem/SoloCanteenManagementSystemApplication.java
@@ -1,9 +1,12 @@
 package com.don.solocanteenmanagementsystem;
 
+import com.don.solocanteenmanagementsystem.customer.config.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class SoloCanteenManagementSystemApplication {
 
     public static void main(String[] args) {
